f42ccf81f0a0c97d48305847e733b6a4eea5eadb
amjunha106 <amjunha106@gmail.com>
2025-01-17 04:20:20 +0900

    25/01/17

---
+++ src/main/java/com/web/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.web.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // http://localhost:8080/KatsuGuide
+    @GetMapping("/KatsuGuide")
+    public String home(Model model) {
+        return "Home"; //
+    }
+}
---
+++ src/main/java/com/web/demo/Map1Application.java
@@ -0,0 +1,16 @@
+package com.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // 스프링 부트 애플리케이션 설정
+public class Map1Application {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(Map1Application.class, args); // 예시, Spring Boot인 경우
+		} catch (Exception e) {
+			e.printStackTrace(); // 예외를 출력하여 원인 파악
+		}
+	}
+}
